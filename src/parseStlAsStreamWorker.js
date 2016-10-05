ccfa72dbe13253802c325ba15831aac188d9c2ce
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-05 17:03:58 +0200

    feat(extra duplex stream for better output)

--- src/parseStlAsStreamWorker.js
+++ src/parseStlAsStreamWorker.js
@@ -25,6 +25,78 @@ export default function parseStlAsStreamWorker (fileReaderStream, files) {
     callback(null, chunk)
   }
 
+
+  const Duplex = require('stream').Duplex
+  class Formatter extends Duplex {
+    constructor () {
+      super({readableObjectMode:true})
+      this.body = []
+
+      function bufferConcat (parts) {
+        var bufs = []
+        for (var i = 0; i < parts.length; i++) {
+          var p = parts[i]
+          if (Buffer.isBuffer(p)) {
+            bufs.push(p)
+          } else if (isBufferish(p)) {
+            bufs.push(new Buffer(p))
+          } else {
+            bufs.push(new Buffer(String(p)))
+          }
+        }
+        return Buffer.concat(bufs)
+      }
+      function prepareResult(data){
+        let positions = data.slice(0, data.length / 2)
+        let normals = data.slice(data.length / 2)
+
+        positions = new Float32Array(positions.buffer.slice(positions.byteOffset, positions.byteOffset + positions.byteLength)) //
+        normals = new Float32Array(normals.buffer.slice(normals.byteOffset, normals.byteOffset + normals.byteLength))
+        return {positions, normals}
+      }
+      this.on('finish', function () {
+        const result = prepareResult(bufferConcat(this.body))
+        console.log('all done', result)
+        this.push(result)
+        this.emit('end')
+       })
+    }
+
+    _read(size) {
+    //  console.log('read')
+    }
+    _write(chunk, encoding, callback) {
+      console.log('foo',chunk)
+      this.body.push(chunk)
+      callback()
+    }
+    end(chunk, encoding, callback){
+      console.log('end', chunk ,encoding, callback)
+      this.emit('finish')
+    }
+  }
+  /*function makeFormatter(){
+    let body = []
+    const f =  Duplex({
+      writableObjectMode : true
+      read(size) {
+        console.log('read', size)
+      },
+      write(chunk, encoding, callback) {
+        console.log('foo', body)
+        body.push(chunk)
+        callback()
+      },
+      end(){
+        console.log(end)
+      }
+    })
+    return f
+  }*/
+
+  const formatter = new Formatter()// makeFormatter()
+
+
   const fileStream = fileReaderStream(files[0], {chunkSize: chunkSize * chunkSize})
   const wokerStreamer = workerStreamParser()
   startTime = new Date()
@@ -33,7 +105,13 @@ export default function parseStlAsStreamWorker (fileReaderStream, files) {
     //.pipe(through2(before))
     .pipe(wokerStreamer)
     //.pipe(through2(after))
+    .pipe(formatter)
+    .pipe(through2({ objectMode: true}, after))
     .pipe(concat(function (data) {
+      console.log('DONE', data)
+    }))
+
+    /*.pipe(concat(function (data) {
       //console.log('FUUUUend of data',data)
       accPositions = data.slice(0, data.length / 2)
       accNormals = data.slice(data.length / 2)
@@ -47,8 +125,9 @@ export default function parseStlAsStreamWorker (fileReaderStream, files) {
       // accPositions = new Float32Array(accPositions.buffer, accPositions.byteOffset, accPositions.byteLength / Float32Array.BYTES_PER_ELEMENT)
       // accNormals = new Float32Array(accNormals.buffer, accNormals.byteOffset, accNormals.byteLength / Float32Array.BYTES_PER_ELEMENT)
 
-    }))
-  pipeline.on('finish', function () {
+    }))*/
+
+  /*pipeline.on('finish', function () {
     accData = {
       positions: accPositions,
       normals: accNormals
@@ -58,5 +137,5 @@ export default function parseStlAsStreamWorker (fileReaderStream, files) {
     //console.log('done', accData)
     endTime = new Date()
     console.log(`Mode: streaming, worker, chunkSize: ${chunkSize}kb, elapsed: ${endTime - startTime}, geometry size: ${accData.positions.byteLength}`)
-  })
+  })*/
 }
